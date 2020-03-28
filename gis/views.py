6bcb47a200e9e13d2cdacb31f10e17fb0e2e3e4d
Sviatoslav Prudnik <prudnik1201510@gmail.com>
2020-03-28 13:18:35 +0200

    Search was created (by Google).

--- gis/views.py
+++ gis/views.py
@@ -22,3 +22,7 @@ def about(request):
 def show_articles(request, article_id):
     article = get_object_or_404(Articles, id=article_id)
     return render(request, 'gis/article.html', {'article': article})
+
+
+def map(request):
+    return render(request, 'gis/map.html')
